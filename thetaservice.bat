3a55f056dbf39121e4bd2359dfca577d0f0424ab
FrenchCommando <martialren@gmail.com>
2025-09-09 12:31:53 -0400

    custom browser for web service

--- thetaservice.bat
+++ thetaservice.bat
@@ -1,4 +1,6 @@
 start %USERPROFILE%\spot-fixings\runthetadata.bat
 start %USERPROFILE%\spot-fixings\runthetadata3.bat
 start %USERPROFILE%\spot-fixings\service.bat
-start chrome "http:\\localhost:5000"
+rem set browser_value=chrome
+set browser_value=firefox
+start %browser_value% "http:\\localhost:5000"
