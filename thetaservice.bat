c4f5762cda177597f3c279f7f7139cb252542832
FrenchCommando <martialren@gmail.com>
2026-02-23 15:19:11 -0500

    calling earnings service

--- thetaservice.bat
+++ thetaservice.bat
@@ -1,6 +1,8 @@
 start %USERPROFILE%\spot-fixings\runthetadata.bat
 start %USERPROFILE%\spot-fixings\runthetadata3.bat
 start %USERPROFILE%\spot-fixings\service.bat
+start %USERPROFILE%\earnings-service\service.bat
 rem set browser_value=chrome
 set browser_value=firefox
 start %browser_value% "http:\\localhost:5000"
+start %browser_value% "http:\\localhost:5001"
